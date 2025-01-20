838bb65c5aa300e30134daa8b70150df02e95580
erfanghorbanee <agh.business@proton.me>
2025-01-20 15:42:17 +0100

    Prevent downloading duplicate files

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 from mimetypes import guess_extension
-
 from dotenv import load_dotenv
 from telethon import TelegramClient, sync
 from telethon.tl.types import MessageMediaPhoto
@@ -14,7 +13,6 @@ API_HASH = os.getenv("TELEGRAM_API_HASH")
 
 # Initialize the Telegram client
 client = TelegramClient("session_name", API_ID, API_HASH)
-
 
 # Function to download files
 def download_files(channel_id, file_format=None, output_dir="."):
@@ -29,21 +27,33 @@ def download_files(channel_id, file_format=None, output_dir="."):
             if message.media:
                 if isinstance(message.media, MessageMediaPhoto):
                     if not file_format or file_format.lower() in ["image", "images"]:
-                        file_path = client.download_media(message, file=output_dir)
-                        print(f"Downloaded image: {file_path}")
+                        filename = message.id + ".jpg"
+                        file_path = os.path.join(output_dir, filename)
+                        if not os.path.exists(file_path):
+                            file_path = client.download_media(message, file=file_path)
+                            print(f"Downloaded image: {file_path}")
+                        else:
+                            print(f"Image already exists: {file_path}")
 
                 elif message.file:
                     mime_type = message.file.mime_type
                     extension = guess_extension(mime_type) if mime_type else ""
+                    filename = message.file.name or str(message.id)
+                    if extension and not filename.endswith(extension):
+                        filename += extension
+
+                    file_path = os.path.join(output_dir, filename)
 
                     if not file_format or (
                         extension and file_format.lower() in extension.lower()
                     ):
-                        file_path = client.download_media(message, file=output_dir)
-                        print(
-                            f"Downloaded {extension if extension else 'file'}: {file_path}"
-                        )
-
+                        if not os.path.exists(file_path):
+                            file_path = client.download_media(message, file=file_path)
+                            print(
+                                f"Downloaded {extension if extension else 'file'}: {file_path}"
+                            )
+                        else:
+                            print(f"File already exists: {file_path}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
